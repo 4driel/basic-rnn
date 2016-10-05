56de78220e82efc0226e4620d00b831107e3120c
LOERA LARRANAGA <jesus.loera@philips.com>
2016-10-04 17:04:19 -0700

    rnn.py update 4/oct/16 17:04

--- rnn.py
+++ rnn.py
@@ -2,7 +2,7 @@ import numpy as np
 
 # string_2_oneHot
 # returns a one-hot vector of string #
-###################################################
+######################################
 def string_2_oneHot(string):
     vocab = list(set(string)) #entire character set used on string
     oneHot = np.zeros((len(string), len(vocab)))
@@ -18,45 +18,44 @@ def string_2_oneHot(string):
 # calculates the softmax function to the recieved vector #
 ##########################################################
 def softmax(x):
-    	e_x = np.exp(x - np.max(x))
-    	return e_x / e_x.sum()
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
 
 # rNewtwork
 # models rnn - character model like#
 ####################################
 class rNetwork(object):
+    def __init__(self, string, vocab):
+	#Hyper params
+	self.iL = len(list(set(string))) #input size of string
+	self.hL = 10
+	self.oL = self.iL #input and output layers same size
 
-	def __init__(self, string, vocab):
-		#Hyper params
-		self.iL = len(list(set(string))) #input size of string
-		self.hL = 10
-		self.oL = self.iL #input and output layers same size
+	#Weights
+	self.W_ih = np.random.uniform(-np.sqrt(1/self.iL), -np.sqrt(1/self.iL), (self.iL, self.hL))
+	self.W_hh = np.random.uniform(-np.sqrt(1/self.hL), -np.sqrt(1/self.hL), (self.hL, self.hL))
+	self.W_ho = np.random.uniform(-np.sqrt(1/self.hL), -np.sqrt(1/self.hL), (self.hL, self.oL))
 
-		#Weights
-		self.W_ih = np.random.uniform(-np.sqrt(1/self.iL), -np.sqrt(1/self.iL), (self.iL, self.hL))
-		self.W_hh = np.random.uniform(-np.sqrt(1/self.hL), -np.sqrt(1/self.hL), (self.hL, self.hL))
-		self.W_ho = np.random.uniform(-np.sqrt(1/self.hL), -np.sqrt(1/self.hL), (self.hL, self.oL))
+    def forward(self, x):
+	hS = np.zeros((len(x), self.hL)) #size x array lenght, hidden layer size
+	hS[-1] = np.zeros(self.hL) #initial hidden state
 
-	def forward(self, x):
-		hS = np.zeros((len(x), self.hL)) #size x array lenght, hidden layer size
-		hS[-1] = np.zeros(self.hL) #initial hidden state
+	o = np.zeros((len(x), self.oL))
 
-		o = np.zeros((len(x), self.oL))
+	for t in np.arange(len(x)):
+	    hS[t] = np.tanh(np.dot(x[t], self.W_ih) + np.dot(hS[t-1], self.W_hh))
+	    o[t]  = softmax(np.dot(hS[t], self.W_ho))
+	return [o, hS]
 
-		for t in np.arange(len(x)):
-			hS[t] = np.tanh(np.dot(x[t], self.W_ih) + np.dot(hS[t-1], self.W_hh))
-			o[t]  = softmax(np.dot(hS[t], self.W_ho))
-		return [o, hS]
+    def lossFunc(self, x):
+	L = 0
 
-	def lossFunc(self, x):
-		L = 0
+	o, s = self.forward(x)
 
-		o, s = self.forward(x)
+	for i in np.arange(len(x)):
+	    L += x[i+1]*np.log(o[i])
 
-		for i in np.arange(len(x)):
-			L += x[i+1]*np.log(o[i])
-
-		L = -L/len(x)
+	    L = -L/len(x)
 
 string = "hello"
 np.random.seed(10)
@@ -66,5 +65,3 @@ x = string_2_oneHot(string)
 o, s = rnn.forward(x[0:(len(x)-1)])
 print o
 print s
-
-
